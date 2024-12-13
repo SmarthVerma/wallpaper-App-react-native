55084e1d8063e878c3d8fdf0fb0e3129d9a46bd5
SmarthVerma <smarthverma2003@gmail.com>
2024-12-14 02:37:49 +0530

    added stack navigation

--- app/(test)/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Slot } from "expo-router";
-import { SafeAreaView, Text } from "react-native";
-
-export default function Layout() {
-    return <SafeAreaView>
-        <Text style={{ backgroundColor: "red" }} >Go back</Text>
-        <Slot />
-    </SafeAreaView>
-}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,18 @@
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-    return <GestureHandlerRootView>
-        <Slot />
-    </GestureHandlerRootView>
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <SafeAreaView style={{ flex: 1 }}>
+                <Stack screenOptions={{ headerShown: false }}>
+                    <Stack.Screen
+                        name="(noBottomBar)/accountInfo"
+                        options={{ headerShown: true, headerTitle: "AccountInfo", headerBackTitle: "Go back" }}
+                    />
+                </Stack>
+            </SafeAreaView>
+        </GestureHandlerRootView>
+    );
 }
